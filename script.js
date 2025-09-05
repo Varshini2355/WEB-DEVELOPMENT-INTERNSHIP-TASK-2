c973c2fc4e0e4877f9bf5509cc3dfb88ca357b63
KS Varshini <varshiniks2355@gmail.com>
2025-09-05 23:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("myButton");
+
+    
+    button.addEventListener("click", function() {
+      
+      button.style.backgroundColor = "lightgreen";
+      button.style.color = "white"; 
+    });
+    
